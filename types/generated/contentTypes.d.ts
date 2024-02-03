bc17d7b8530496911d1b833730e20158b543ad8f
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-02-03 22:01:18 +0400

    created one instance in dev mode to test production mode of strapi in digital ocean

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,37 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiSliderImagesHeadingSliderImagesHeading
+  extends Schema.CollectionType {
+  collectionName: 'slider_images_headings';
+  info: {
+    singularName: 'slider-images-heading';
+    pluralName: 'slider-images-headings';
+    displayName: 'slider-images-heading';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    slider_heading: Attribute.String & Attribute.Required;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::slider-images-heading.slider-images-heading',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::slider-images-heading.slider-images-heading',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -791,6 +822,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::slider-images-heading.slider-images-heading': ApiSliderImagesHeadingSliderImagesHeading;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
