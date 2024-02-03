af5a294b69a9e0f67fb9e6cb593f0e3e755e4b93
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-02-04 01:00:01 +0400

    created new plugins.js file inside config/env/production, moved content of default plugins.js to prod plugins.js

---
+++ config/env/production/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@lyl-radio/strapi-provider-upload-do',
+      providerOptions: {
+        key: env('DO_SPACE_ACCESS_KEY'),
+        secret: env('DO_SPACE_SECRET_KEY'),
+        endpoint: env('DO_SPACE_ENDPOINT'),
+        space: env('DO_SPACE_BUCKET')
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
